d4734e37f7a50db7609683405125af43fe321208
Jenifer Cochran <Jenifer.Cochran@rgi-corp.com>
2015-10-19 19:37:03 -0400

    adding indexer comments

--- Assignment7SWE619/src/search/Indexer.java
+++ Assignment7SWE619/src/search/Indexer.java
@@ -2,7 +2,21 @@ package search;
 
 public interface Indexer<E>
 {
+	/**
+	 * Effects: returns the index of the first occurrence of the
+	 *  specified element in the collection or -1 if this collection
+	 *  does not contain the element. 
+	 *  
+	 *  @throws NullPointerException if the specified element is null
+	 * @param element
+	 * @return
+	 */
     public int indexOf(E element);
     
+    /**
+     * Effects: returns the number of elements in this collection. 
+     *
+     * @return returns the number of elements in this collection.
+     */
     public int size();
 }
