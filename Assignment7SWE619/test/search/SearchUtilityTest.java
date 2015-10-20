57297d8fb2601180007c9afaebadedebaa63c0da
ErichdotPy <eosaben@masonlive.gmu.edu>
2015-10-19 22:44:55 -0400

    NPE Tests
    NPE tests for SearchUtility and VectorIndexer

--- Assignment7SWE619/test/search/SearchUtilityTest.java
+++ Assignment7SWE619/test/search/SearchUtilityTest.java
@@ -2,14 +2,35 @@ package search;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class SearchUtilityTest {
+	
+	
 
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	
 	@Test
-	public void test() 
+	public void testSearch() 
 	{
-		fail("Not yet implemented");
+		
+	}
+	
+	@Test(expected=NullPointerException.class)
+	public void testNPE() {
+		SearchUtility.search(null,"a");
 	}
 
 }
--- Assignment7SWE619/test/search/VectorIndexerTest.java
+++ Assignment7SWE619/test/search/VectorIndexerTest.java
@@ -1,6 +1,43 @@
 package search;
 
+<<<<<<< Updated upstream
 public class VectorIndexerTest 
 {
+=======
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class VectorIndexerTest {
+	VectorIndexer<String> v1;
+	
+	@Before
+	public void setUp() throws Exception {
+		List<String> l = new ArrayList();
+
+		v1 = new VectorIndexer(l);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+	}
+	
+
+	@Test(expected=NullPointerException.class)
+	public void testIndexOfNPE() {
+		v1.indexOf(null);
+		
+	}
+
+>>>>>>> Stashed changes
 
 }
