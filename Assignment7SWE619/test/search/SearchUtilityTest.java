5bbb6d3accbf55308afd20613722b654cad95817
ErichdotPy <eosaben@masonlive.gmu.edu>
2015-10-20 23:22:06 -0400

    JUnit Tests
    Unit tests for SearchUtilityTest.java and VectorIndexerTest.java

--- Assignment7SWE619/test/search/SearchUtilityTest.java
+++ Assignment7SWE619/test/search/SearchUtilityTest.java
@@ -1,36 +1,56 @@
 package search;
 
+/**
+ * 
+ * @author Jenifer Cochran 
+ * @author Erich O'Saben
+ *
+ */
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class SearchUtilityTest {
-	
+	VectorIndexer<String> v1;
 	
 
+	
 	@Before
 	public void setUp() throws Exception {
+		List<String> l = new ArrayList();
+		l.add("a");
+		l.add("b");
+		l.add("c");
+		
+		
+		v1 = new VectorIndexer<String>(l);
 	}
 
-	@After
-	public void tearDown() throws Exception {
-	}
-	
+		
 	
 	@Test
 	public void testSearch() 
 	{
+		assertEquals(0, SearchUtility.search(v1, "a"));
+		assertEquals(-1, SearchUtility.search(v1, "d"));
 		
 	}
 	
 	@Test(expected=NullPointerException.class)
-	public void testNPE() {
+	public void testColletionNPE() {
 		SearchUtility.search(null,"a");
+	}
+	
+
+	@Test(expected=NullPointerException.class)
+	public void testElementNPE() {
+		
+		//the element being searched for is null
+		SearchUtility.search(v1,null);
 	}
 
 }
--- Assignment7SWE619/test/search/VectorIndexerTest.java
+++ Assignment7SWE619/test/search/VectorIndexerTest.java
@@ -1,33 +1,71 @@
 package search;
 
+/**
+ * 
+ * @author Jenifer Cochran 
+ * @author Erich O'Saben
+ *
+ */
+
 import static org.junit.Assert.*;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.List;
 
+import java.util.List;
 
 public class VectorIndexerTest {
 	VectorIndexer<String> v1;
+	VectorIndexer<String> v2;
 	
 	@Before
 	public void setUp() throws Exception {
 		List<String> l = new ArrayList();
-
+				
+		l.add("a");
+		l.add("b");
+		l.add("c");
+		
 		v1 = new VectorIndexer(l);
+		v2 = new VectorIndexer("d", "e", "f", "g");
 	}
 
-	@After
-	public void tearDown() throws Exception {
-	}
-
+	
 	@Test
-	public void test() {
+	public void testElipsesConstructor(){
+		//shows that the Ellipses constructor and the "list" constructor create equivalent objects
+		VectorIndexer<String> v3 = new VectorIndexer("a", "b", "c");
+		
+		assertEquals(v1.indexOf("a"),v3.indexOf("a"));
+		assertEquals(v1.indexOf("b"),v3.indexOf("b"));
+		assertEquals(v1.indexOf("c"),v3.indexOf("c"));
+		
+		assertEquals(v1.size(),v3.size());
+		
+		
+		
 	}
 	
+	
+	@Test
+	public void testIndexOf(){
+		//search for items in V.I.
+		assertEquals(0,v1.indexOf("a"));
+		assertEquals(0, v2.indexOf("d"));
+		
+		//search for items not in V.I.
+		assertEquals(-1,v1.indexOf("d"));
+		assertEquals(-1, v2.indexOf("a"));
+		
+	}
+	
+	@Test
+	public void testsize(){
+		assertEquals(3,v1.size());
+		assertEquals(4, v2.size());
+	}
 
 	@Test(expected=NullPointerException.class)
 	public void testIndexOfNPE() {
