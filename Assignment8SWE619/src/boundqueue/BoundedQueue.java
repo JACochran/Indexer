597542641810b55e10bf22af9cf509fd152c5b58
Jenifer Cochran <Jenifer.Cochran@rgi-corp.com>
2015-10-26 19:47:43 -0400

    adding book code and generifying

--- Assignment8SWE619/src/boundqueue/BoundedQueue.java
+++ Assignment8SWE619/src/boundqueue/BoundedQueue.java
@@ -1,5 +1,81 @@
 package boundqueue;
 
-public class BoundedQueue {
+import java.util.Collection;
 
+public class BoundedQueue<E>
+{
+	protected E[] rep;
+	protected int front = 0;
+	protected int back = -1;
+	protected int size = 0;
+	protected int count = 0;
+	
+	@SuppressWarnings("unchecked")
+	public BoundedQueue(int size) 
+	{
+		if (size > 0) 
+		{
+			this.size = size;
+			rep = (E[]) new Object[size];
+			back = size - 1;
+		}
+	}
+
+	public boolean isEmpty()
+	{
+		return (count == 0);
+	}
+
+	public boolean isFull()
+	{
+		return (count == size);
+	}
+
+	public int getCount()
+	{
+		return count;
+	}
+
+	public void put(E e) 
+	{
+		if (e != null && !isFull()) 
+		{
+			back++;
+			if (back >= size)
+				back = 0;
+			rep[back] = e;
+			count++;
+		}
+	}
+
+	public E get()
+	{
+		E result = null;
+		if (!isEmpty()) 
+		{
+			result = rep[front];
+			rep[front] = null;
+			front++;
+			if (front >= size)
+				front = 0;
+			count--;
+		}
+		return result;
+	}
+	
+	public void getAll(Collection<? super E> destination)
+	{
+		while(!isEmpty())
+		{
+			destination.add(get());
+		}
+	}
+	
+	public void putAll(Iterable<? extends E> values)
+	{
+		for(E value: values)
+		{
+			put(value);
+		}
+	}
 }
