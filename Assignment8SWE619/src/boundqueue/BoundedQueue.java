ddcb0d735f2374682763de908bd3cf4dd5695e4e
Jenifer Cochran <Jenifer.Cochran@rgi-corp.com>
2015-10-26 21:16:12 -0400

    adding javadoc

--- Assignment8SWE619/src/boundqueue/BoundedQueue.java
+++ Assignment8SWE619/src/boundqueue/BoundedQueue.java
@@ -1,68 +1,97 @@
 package boundqueue;
 
 import java.util.Collection;
-
+import java.util.concurrent.LinkedBlockingQueue;
+/**
+ * 
+ * @author Jenifer Cochran
+ * @author Erich O'Saben
+ *
+ * @param <E>
+ */
 public class BoundedQueue<E>
 {
-	protected E[] rep;
-	protected int front = 0;
-	protected int back = -1;
-	protected int size = 0;
-	protected int count = 0;
-	
-	@SuppressWarnings("unchecked")
+	private LinkedBlockingQueue<E> rep;
+
+	/**
+	 * BoundedQueue Creates a BoundedQueue with the given (fixed) capacity.
+	 * 
+	 * @throws IllegalArgumentException
+	 *             - if size is not greater than zero
+	 * @param size
+	 *            the capacity of this Bounded queue
+	 */
 	public BoundedQueue(int size) 
 	{
-		if (size > 0) 
-		{
-			this.size = size;
-			rep = (E[]) new Object[size];
-			back = size - 1;
-		}
+	    rep = new LinkedBlockingQueue<E>(size);
 	}
 
+	/**
+	 * Returns true if this collection contains no elements. This implementation
+	 * returns size() == 0.
+	 * 
+	 * @return true if there are no elements in this bounded Queue
+	 */
 	public boolean isEmpty()
 	{
-		return (count == 0);
+		return rep.isEmpty();
 	}
 
+	/**
+	 * Returns true if the BoundedQueue has reached its capacity
+	 * 
+	 * @return true if the BoundedQueue has reached its capacity, false if there
+	 *         is still space left to add more elements
+	 */
 	public boolean isFull()
 	{
-		return (count == size);
+		return rep.remainingCapacity() == 0;
 	}
 
+	/**
+	 * Returns the number of elements in the BoundedQueue
+	 * 
+	 * @return the number of elements in the BoundedQueue
+	 */
 	public int getCount()
 	{
-		return count;
+		return rep.size();
 	}
 
-	public void put(E e) 
+	/**
+	 * Adds the parameter element to this BoundedQueue if the Queue is not full
+	 * and the element is not null.
+	 * 
+	 * @param element
+	 *            element to be added to this BoundedQueue
+	 */
+	public void put(E element) 
 	{
-		if (e != null && !isFull()) 
+		if (element != null && !isFull()) 
 		{
-			back++;
-			if (back >= size)
-				back = 0;
-			rep[back] = e;
-			count++;
+			rep.add(element);
 		}
-	}
-
-	public E get()
-	{
-		E result = null;
-		if (!isEmpty()) 
-		{
-			result = rep[front];
-			rep[front] = null;
-			front++;
-			if (front >= size)
-				front = 0;
-			count--;
-		}
-		return result;
 	}
 	
+	/**
+	 * Retrieves and removes the head of this queue, or returns null if this
+	 * queue is empty
+	 * 
+	 * @return retrieves and removes the head of this BoundedQueue, or returns
+	 *         null if this is empty
+	 */
+	public E get()
+	{
+		return rep.poll();
+	}
+
+	/**
+	 * Will retrieve and remove all elements from this and add them to the
+	 * destination parameter.
+	 * 
+	 * @param destination
+	 *            the collection that all the elements will be added to
+	 */
 	public void getAll(Collection<? super E> destination)
 	{
 		while(!isEmpty())
@@ -71,6 +100,13 @@ public class BoundedQueue<E>
 		}
 	}
 	
+	/**
+	 * Will add the elements in the parameter values to this BoundedQueue until
+	 * this reaches capacity and if the element in values is not null.
+	 * 
+	 * @param values
+	 *            the values to be added to this
+	 */
 	public void putAll(Iterable<? extends E> values)
 	{
 		for(E value: values)
