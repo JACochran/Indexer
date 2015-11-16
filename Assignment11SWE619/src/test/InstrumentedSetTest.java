b75b760b60ff5e6bd6e4eff112f2544956b8796e
ErichdotPy <eosaben@masonlive.gmu.edu>
2015-11-16 16:39:43 -0500

    Initial Commit
    I updated InstrumentedSet.java to the a Collection which required a
    cast to a Set on line 10, in the constructor. I wasn’t sure if we
    needed to also change ForwardingSet to a collection so I left that as
    is.

---
+++ Assignment11SWE619/src/test/InstrumentedSetTest.java
@@ -0,0 +1,20 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class InstrumentedSetTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
