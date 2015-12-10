ad6f5c735c8a3f564dbc5e87e34e1c1b8ec85054
Jenifer Cochran <Jenifer.Cochran@rgi-corp.com>
2015-12-09 19:17:09 -0500

    todays stock earnings

--- Assignment12v2/src/stock/Portfolio.java
+++ Assignment12v2/src/stock/Portfolio.java
@@ -38,12 +38,12 @@ public class Portfolio {
    }
 	
 	public boolean buyAdditionalStock(double percent, Stock stock) {
-		if (percent <= 0.0 || percent > 100.0 )
+		if (percent <= 0.0 || percent > 1.0 )
 			throw new IllegalArgumentException ("Percent must be between 0 and 100");
 		
 		int volume = this.stockService.getVolume(stock);
 		int quantity = (int) (volume * percent);
-		double totalCost = quantity * stockService.getOpenPrice(stock);
+		double totalCost = quantity * stockService.getPrice(stock);
 		
 		if (totalCost > accountBalance )
 			return false;
@@ -56,9 +56,20 @@ public class Portfolio {
 			else{
 				this.stocks.add(new Stock(stock.getStockId(), stock.getTicker(), quantity));
 			}
-			
 			return true;
 		}	
+	}
+	
+	public double todaysStockEarnings()
+	{
+		double earnings = 0;
+		
+		for(Stock stock : this.stocks)
+		{
+			earnings += stock.getQuantity()*(this.stockService.getClosingPrice(stock) - this.stockService.getOpenPrice(stock));
+		}
+		
+		return earnings;
 	}
 
 }
--- Assignment12v2/test/stock/EasyMockPortfolioTest.java
+++ Assignment12v2/test/stock/EasyMockPortfolioTest.java
@@ -10,45 +10,93 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class EasyMockPortfolioTest {
-	  Portfolio portfolio;	
-	  StockService stockService;
+	Portfolio portfolio;
+	StockService stockService;
 
-		
 	@Before
 	public void setUp() throws Exception {
-		//Create a portfolio object which is to be tested		
-		portfolio = new Portfolio();		
-      
-      //Create the mock object of stock service
-      stockService = EasyMock.createMock(StockService.class);		
-      
-      //set the stockService to the portfolio
-      portfolio.setStockService(stockService);
+		// Create a portfolio object which is to be tested
+		portfolio = new Portfolio();
+
+		// Create the mock object of stock service
+		stockService = EasyMock.createMock(StockService.class);
+
+		// set the stockService to the portfolio
+		portfolio.setStockService(stockService);
 	}
 
 	@Test
 	public void exampleTest() {
-		//Creates a list of stocks to be added to the portfolio
-	      List<Stock> stocks = new ArrayList<Stock>();
-	      Stock googleStock = new Stock("1","Google", 10);
-	      Stock microsoftStock = new Stock("2","Microsoft",100);	
-	      
-	      stocks.add(googleStock);
-	      stocks.add(microsoftStock);
+		// Creates a list of stocks to be added to the portfolio
+		List<Stock> stocks = new ArrayList<Stock>();
+		Stock googleStock = new Stock("1", "Google", 10);
+		Stock microsoftStock = new Stock("2", "Microsoft", 100);
 
-	      //add stocks to the portfolio
-	      portfolio.setStocks(stocks);
+		stocks.add(googleStock);
+		stocks.add(microsoftStock);
 
-	      // mock the behavior of stock service to return the value of various stocks
-	      EasyMock.expect(stockService.getPrice(googleStock)).andReturn(50.00);
-	      EasyMock.expect(stockService.getPrice(microsoftStock)).andReturn(1000.00);		
+		// add stocks to the portfolio
+		portfolio.setStocks(stocks);
 
-	      // activate the mock
-	      EasyMock.replay(stockService);		
+		// mock the behavior of stock service to return the value of various
+		// stocks
+		EasyMock.expect(stockService.getPrice(googleStock)).andReturn(50.00);
+		EasyMock.expect(stockService.getPrice(microsoftStock)).andReturn(1000.00);
 
-	      double marketValue = portfolio.getMarketValue();		
-	      assertTrue( marketValue == 100500.0);
-	       
+		// activate the mock
+		EasyMock.replay(stockService);
+
+		double marketValue = portfolio.getMarketValue();
+		assertTrue(marketValue == 100500.0);
+	}
+
+	@Test
+	public void getMoreStocks() {
+		// Creates a list of stocks to be added to the portfolio
+		List<Stock> stocks = new ArrayList<Stock>();
+		Stock googleStock = new Stock("1", "Google", 10);
+		Stock microsoftStock = new Stock("2", "Microsoft", 100);
+
+		stocks.add(googleStock);
+		// add stocks to portfolio (exclude microsoft)
+		portfolio.setStocks(stocks);
+		// set up return values
+		EasyMock.expect(stockService.getPrice(googleStock)).andReturn(35.00).times(1);
+		EasyMock.expect(stockService.getPrice(microsoftStock)).andReturn(57.0).times(2);
+
+		EasyMock.expect(stockService.getVolume(googleStock)).andReturn(100).times(1);
+		EasyMock.expect(stockService.getVolume(microsoftStock)).andReturn(900).times(2);
+		// activate!
+		EasyMock.replay(stockService);
+		// buy the stock, and ensure it returns true
+		assertTrue(portfolio.buyAdditionalStock(0.01, microsoftStock));
+		assertTrue(portfolio.buyAdditionalStock(0.07, googleStock));
+		// buy more than you have money for! should be unsuccessful
+		assertFalse(portfolio.buyAdditionalStock(0.99, microsoftStock));
+	}
+
+	@Test
+	public void stockEarnings() {
+		// Creates a list of stocks to be added to the portfolio
+		List<Stock> stocks = new ArrayList<Stock>();
+		Stock googleStock = new Stock("1", "Google", 10);
+		Stock microsoftStock = new Stock("2", "Microsoft", 100);
+
+		stocks.add(googleStock);
+		stocks.add(microsoftStock);
+
+		portfolio.setStocks(stocks);
+
+		// set up return values
+		EasyMock.expect(stockService.getOpenPrice(googleStock)).andReturn(35.00).times(1);
+		EasyMock.expect(stockService.getOpenPrice(microsoftStock)).andReturn(57.0).times(1);
+
+		EasyMock.expect(stockService.getClosingPrice(googleStock)).andReturn(22.0).times(1);
+		EasyMock.expect(stockService.getClosingPrice(microsoftStock)).andReturn(70.0).times(1);
+		// activate!
+		EasyMock.replay(stockService);
+		
+		assertTrue(1170.0 == portfolio.todaysStockEarnings());
 	}
 
 }
